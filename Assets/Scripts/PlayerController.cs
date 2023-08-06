0f53d2f87acb5a0ce67dc64e52dc4be602294a9c
wjdahdgus123@gmail.com <wjdahdgus123@gmail.com>
2023-08-07 02:12:39 +0900

    feat : Player 구현

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
+    public Rigidbody playerRigidbody;
     public float speed = 8f;
-    // Start is called before the first frame update
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
