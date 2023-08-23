07c082407e5f47b7ed19469f5fcb5622120c06a3
wjdahdgus123@gmail.com <wjdahdgus123@gmail.com>
2023-08-23 22:49:53 +0900

    fix : Bullet 충돌수정

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public float speed = 8f;
+    private Rigidbody bulletRigidbody;
     // Start is called before the first frame update
+    private void Awake()
+    {
+        bulletRigidbody = GetComponent<Rigidbody>();
+    }
     void Start()
     {
-        
+        bulletRigidbody.velocity = transform.forward * speed;
+
+        Destroy(gameObject, 3f);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+        PlayerController playerController = other.GetComponent<PlayerController>();
+            if (playerController != null)
+            {
+                playerController.Die();
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
